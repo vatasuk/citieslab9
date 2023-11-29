33d8a1e801d0c31fcd74f1669ab3160c2a0dcbe7
vatasuk <vatasuk111@gmail.com>
2023-11-29 21:28:31 +0300

    3

---
+++ src/main/java/lab9/citieslab9/City.java
@@ -0,0 +1,51 @@
+package lab9.citieslab9;
+
+public class City {
+    private int pop, id;
+    private String name;
+
+    public City() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "pop=" + pop +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getPop() {
+        return pop;
+    }
+
+    public void setPop(int pop) {
+        this.pop = pop;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public City(int id, String name, int pop) {
+        this.pop = pop;
+        this.id = id;
+        this.name = name;
+    }
+
+}
